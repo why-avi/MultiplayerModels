11b66e4a98bc6143930dc36219e03ebebfd90881
Tyler S <78190945+T28ST@users.noreply.github.com>
2026-03-27 13:47:56 -0400

    working on canvas

--- mpModels/src/game/render.js
+++ mpModels/src/game/render.js
@@ -2,3 +2,9 @@
  * Renderer for each simulation panel.
  * Wraps a canvas element and provides a draw method for each frame.
  */
+
+export class Render {
+    constructor(canvas) {
+        const ctx = canvas.getContext('2d');
+    }
+}
---
+++ mpModels/src/main.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+const element = (id) => {
+    return document.getElementById(id);
+}
+
