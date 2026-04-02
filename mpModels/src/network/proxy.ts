41b8efedc758c1ff75f0b001f8af0e6e1fcf0e46
Tyler S <tyler@notnats.com>
2026-04-02 17:26:36 -0400

    Finalized Proxy object. All game clients create a proxy object to send and recieve messages. Exact message content depends on sync type being simulated. Latency adds a delay to arrival time to simulate travel time. Loss rate is a number between 0 and 1 representing a percent chance that a packet will not be recieved (in this case sent).

--- mpModels/src/network/proxy.ts
+++ mpModels/src/network/proxy.ts
@@ -1,7 +1,42 @@
 /**
- * A Simulated network queue with adjustable settings.
+ * Message queue that with simulated latency and packet loss. Payload messages
+ * may contain information for either snapshot or lockstep sync models.
  */
 
-export class Proxy {
+interface Message {
+    payload: any;
+    arrivalTime: number;
+}
 
+export class Proxy {
+    private messages: Message[];   
+    private lossRate: number; // Number between 0 and 1 representing percent of dropped packets.
+
+    constructor() {
+        this.messages = [];
+        this.lossRate = 0; // Default to no lost packets.
+    }
+
+    // Takes a message from an authoritative server or fellow client and store in a queue.
+    send(payload: any, latency: number) {
+        // Exits process if this packet is "lost"
+        if (Math.random() < this.lossRate) return;
+        this.messages.push({
+            payload: payload,
+            arrivalTime: +new Date() + latency,
+        });
+    }
+
+    // Return latest messages that have "arrived"
+    receive() {
+        const now = +new Date();
+        const arrived = this.messages.filter(message => message.arrivalTime <= now);
+        this.messages = this.messages.filter(message => message.arrivalTime > now);
+        return arrived.map(message => message.payload);
+    }
+
+    // Change the packet loss rate setting.
+    setLossRate(rate: number) {
+        this.lossRate = rate;
+    }
 }
